70b326b497c78129fe4c13873d7ae7e6b75273ca
HWANII\Dae Hwan <dhfg0628@naver.com>
2023-06-25 16:50:37 +0900

    First commit

---
+++ day01/src/day01_basic/Ex01_HelloJava.java
@@ -0,0 +1,21 @@
+package day01_basic; // 패키지 명시
+
+public class Ex01_HelloJava { // 클래스명 : 소스 파일명과 같다.
+	public static void main(String[] args) {
+		// 자바는 main 함수 (x) => main 메소드 (o)
+		
+		// 주석 : C와 같은 방법으로 처리(//, /* */)
+		// 이클립스 주석 자동 완성 : ctrl+/
+		
+		// 실행 ctrl+F11
+		
+		System.out.println("Hello Java World");
+		// print line : 줄바꿈 진행을 해준다. 즉, 엔터키를 쳐주고 끝낸다.
+		System.out.print("Hello Java World");
+		
+		// 자동완성 기능 : ctrl+space
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}	
+
+}
